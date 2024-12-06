184ee3b0e99f3a11d4078169f8375d9ece130aae
AlexandrAshHF <ashevskya@gmail.com>
2024-12-06 13:33:52 +0300

    created base entities

---
+++ src/System/BaseLayers/System.BaseLayers.Core/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace System.BaseLayers.Core.Entities
+{
+    /// <summary>
+    /// Базовая сущность
+    /// </summary>
+    /// <typeparam name="TId">Тип идентификатора</typeparam>
+    public abstract class BaseEntity<TId>
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public TId Id { get; set; }
+    }
+}
